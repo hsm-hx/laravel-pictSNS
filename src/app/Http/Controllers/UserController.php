08b28b019366dbd9e5fd0bd4daccf45457e790c5
hsm-hx <skytea922@gmail.com>
2019-02-14 16:04:13 +0900

    :construction: STEP2-3 課題前まで

---
+++ src/app/Http/Controllers/UserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+
+class UserController extends Controller
+{
+  public function index()
+  {
+    // データの追加
+    $email = substr(str_shuffle('abcdefghijklmnopqrstuvwxyz'), 0, 8) . '@yyyy.com';
+    User::insert(['name' => 'yamada taro', 'email' => $email, 'password' => 'xxxxxxxx']);
+
+    // 全データの取り出し
+    $users = User::all();
+    return view('user', ['users' => $users]);
+  }
+}
